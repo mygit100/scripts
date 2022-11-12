b8f538f9e82fc96ca8fdd0b7fbac3e5bf1cfd783
mygit100 <mygit100@users.noreply.github.com>
2022-11-12 00:35:57 -0500

    Update initiallinux.sh

--- initiallinux.sh
+++ initiallinux.sh
@@ -2,7 +2,7 @@ sudo echo
 
 
 read -p "Enter remote username : " remotename
-read -s -p "Enter $remotename's password : " remotepw
+read -s -p "Enter $remotename password : " remotepw
 egrep "^$remotename" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 	echo "$remotename exists!"
