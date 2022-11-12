7a80fd5f7fced1d1e1bb380746778a42b4d5f271
mygit100 <mygit100@users.noreply.github.com>
2022-11-12 00:00:27 -0500

    Update initiallinux.sh

--- initiallinux.sh
+++ initiallinux.sh
@@ -1,3 +1,12 @@
+sudo echo
+
+read -p "What username would you like to use for remote connection? " remotename
+read -p "Enter a password for remote user." remotepw
+
+echo "Your remote credentials are 
+echo "username - $remotename"
+echo "password - $remotepw"
+
 sudo apt update
 sudo apt install tmux, openssh-server -y 
 sudo apt install xrdp -y
