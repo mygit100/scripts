eb6a2c0b7acc629c5b7781fc1197915efd05837d
mygit100 <mygit100@users.noreply.github.com>
2022-11-12 00:45:08 -0500

    Update initiallinux.sh

--- initiallinux.sh
+++ initiallinux.sh
@@ -1,25 +1,24 @@
-sudo echo
+sudo -i
 
-
-sudo read -p "Enter remote username : " remotename
-sudo read -s -p "Enter $remotename password : " remotepw
+read -p "Enter remote username : " remotename
+read -s -p "Enter $remotename password : " remotepw
 egrep "^$remotename" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 	echo "$remotename exists!"
 	exit 1
 else
 	pass=$(perl -e 'print crypt($ARGV[0], "password")' $remotepw)
-	sudo useradd -m -p "$pass" "$remotename"
+	useradd -m -p "$pass" "$remotename"
 	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 fi
 
-sudo apt update
-sudo apt autoremove
-sudo apt install tmux, openssh-server -y 
-sudo apt install xrdp -y
+apt update
+apt autoremove
+apt install tmux, openssh-server -y 
+apt install xrdp -y
 
-sudo usermod -a -G sudo $remotename
-sudo usermod -a -G libvirt $remotename
-sudo usermod -a -G kvm $remotename
+usermod -a -G sudo $remotename
+usermod -a -G libvirt $remotename
+usermod -a -G kvm $remotename
 
-sudo systemctl restart xrdp
+systemctl restart xrdp
