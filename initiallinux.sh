a8f7c92cef68ced151b6d8d60f4b20e6fe963c4a
mygit100 <mygit100@users.noreply.github.com>
2022-11-12 00:22:20 -0500

    Update initiallinux.sh

--- initiallinux.sh
+++ initiallinux.sh
@@ -1,17 +1,21 @@
 sudo echo
 
-read -p "What username would you like to use for remote connection? " remotename
-read -p "Enter a password for remote user." remotepw
 
-echo "Your remote credentials are 
-echo "username - $remotename"
-echo "password - $remotepw"
+read -p "Enter remote username : " remotename
+read -s -p "Enter $remotename's password : " remotepw
+egrep "^$remotename" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo "$remotename exists!"
+	exit 1
+else
+	pass=$(perl -e 'print crypt($ARGV[0], "password")' $remotepw)
+	sudo useradd -m -p "$pass" "$remotename"
+	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+fi
 
 sudo apt update
 sudo apt install tmux, openssh-server -y 
 sudo apt install xrdp -y
-
-sudo adduser $remotename
 
 sudo usermod -a -G sudo $remotename
 sudo usermod -a -G libvirt $remotename
