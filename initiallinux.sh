c595e772a658edb51c4ebe3a43054b6bc217c8cd
mygit100 <mygit100@users.noreply.github.com>
2022-11-12 00:01:45 -0500

    Update initiallinux.sh

--- initiallinux.sh
+++ initiallinux.sh
@@ -11,8 +11,6 @@ sudo apt update
 sudo apt install tmux, openssh-server -y 
 sudo apt install xrdp -y
 
-# what name do you want to use
-
-sudo adduser reverseme
-sudo usermod -a -G reverset reverseme
+sudo adduser $remotename
+sudo usermod -a -G reverset $remotename
 sudo systemctl restart xrdp
