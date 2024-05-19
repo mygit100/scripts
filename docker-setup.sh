0129240fcb69234970724fdddb57ea89379730f1
mygit100 <mygit100@users.noreply.github.com>
2024-05-19 18:27:47 -0400

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -44,7 +44,7 @@ sudo usermod -aG docker $myusername
 mkdir -p $mypath/docker/portainer_data
 
 # Create and start Portainer container
-docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v $mypath/docker/portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v $mypath/docker/portainer_data:/data portainer/portainer-ce:latest
 
 
 # running local do not need to open ports
